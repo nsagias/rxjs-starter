4373b9cbc6476133f7a8e46e830a5c3ec4b197ab
Nick <nsagias@gmail.com>
2022-12-14 00:16:13 -0500

    add try catch example of rxjs hellow world

---
+++ rxjs-hello-world-try-catch.js
@@ -0,0 +1,32 @@
+import  { Observable } from "@reactivex/rxjs";
+
+const oberservable = new Observable ((subscriber) => {
+  try {
+    subscriber.next(1);
+    subscriber.next(2);
+    subscriber.next(3);
+    setTimeout(() => {
+      subscriber.next(4);
+      subscriber.complete();  // completing inside of promise
+    });
+    // subscriber.complete();
+  } catch (err) {
+    subscriber.error(err); // delivers an error if it caught one
+  }
+});
+
+console.info("BEFORE SUBSCRIBE");
+oberservable.subscribe({
+  next(x) {
+    console.log(`GOT VALUE ${x} ON RXJS HELLO WORLD`);
+  },
+  error(err) {
+    console.error("ERROR ON RXJS HELLO WORLD");
+  },
+  complete() {
+    console.log("RXJS HELLOW WORLD, DONE!");  
+  }
+});
+
+console.info("AFTER SUBSCRIBE");
+
