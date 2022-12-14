f1200264a0551b1baf5c6e51a069743680b709b2
Nick <nsagias@gmail.com>
2022-12-14 00:08:23 -0500

    add rxjs-compat, add type module, create rxjs-hello-world

---
+++ rxjs-hello-world.js
@@ -0,0 +1,26 @@
+import  { Observable } from "@reactivex/rxjs";
+
+const oberservable = new Observable ((subscriber) => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  });
+});
+
+console.info("BEFORE SUBSCRIBE");
+oberservable.subscribe({
+  next(x) {
+    console.log(`GOT VALUE ${x} ON RXJS HELLO WORLD`);
+  },
+  error(err) {
+    console.error("ERROR ON RXJS HELLO WORLD");
+  },
+  complete() {
+    console.log("RXJS HELLOW WORLD, DONE!");  
+  }
+});
+
+console.info("AFTER SUBSCRIBE");
