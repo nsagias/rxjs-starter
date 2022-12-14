638c82a8c048c90c7e754f1f86b3c3b5e39c665a
Nick <nsagias@gmail.com>
2022-12-14 01:36:30 -0500

    add operator first

--- operators/rxjs-map.js
+++ operators/rxjs-map.js
@@ -1,5 +1,10 @@
 import { of } from "rxjs";
-import { map } from "rxjs/operators/index.js";
+import { map, first } from "rxjs/operators/index.js";
+// import pkg1 from "rxjs";
+// import pkg2 from 'rxjs/operators/index.js';
+
+// console.log(pkg1);
+// console.log(pkg2);
 
 const observer = {
   next: x => console.log(`Obeserver got next value of ${x}`),
@@ -8,23 +13,9 @@ const observer = {
 }
 
 of(1, 2, 3)
-.pipe(map((x) => x * x))
+.pipe(first((x) => x * x))
 .subscribe((v) => console.log(`value: ${v}`));
 
-
-// import { of } from "rxjs";
-// import pkg1 from "rxjs";
-
-// import { map } from 'rxjs/operators/index.js';
-// import pkg2 from 'rxjs/operators/index.js';
-
-// // console.log(pkg1);
-// // console.log(pkg2)
-// const observer = {
-//   next: x => console.log(`Obeserver got next value of ${x}`),
-//   error: err => console.error(`Obeserver got the following error ${err}`),
-//   complete:  () => console.log('Observer is complete')
-// }
 
 of(1, 2, 5).pipe(map((x) => x * x))
 .subscribe(observer);
